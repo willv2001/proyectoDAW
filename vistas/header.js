777d545e5bba5dd9358c63831d90f76262b7ee0b
Hazel Azucena Calderón Bonilla <166912386+LezahA@users.noreply.github.com>
2024-05-20 01:08:49 -0600

    cambio en header y footer

---
+++ vistas/header.js
@@ -0,0 +1,23 @@
+function renderSection() {
+    const sectionHTML = `
+      <div class="principal-div text-center">
+        <br><br>
+        <h1 class="display-1">Hospital</h1>
+        <h2 class="display-5">Universidad de El Salvador</h2>
+        <br>
+        <button type="button" class="btn btn-dark">
+          <a class="nav-link" href="./vistas/somos.html">Quienes Somos</a>
+        </button>
+        <br><br>
+      </div>
+    `;
+  
+    const sectionContainer = document.createElement('div');
+    sectionContainer.innerHTML = sectionHTML;
+    document.body.appendChild(sectionContainer);
+  }
+  
+  renderSection(); // Llamada a la función para que se ejecute al cargar la página
+  
+  
+  
