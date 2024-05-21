adfcf33c55d3d66393e4530e8d7b03a5a22824d5
Hazel Azucena Calderón Bonilla <166912386+LezahA@users.noreply.github.com>
2024-05-20 21:50:53 -0600

    cambios para integrar formulario

--- vistas/header.js
+++ vistas/header.js
@@ -1,23 +1,22 @@
 function renderSection() {
-    const sectionHTML = `
-      <div class="principal-div text-center">
-        <br><br>
-        <h1 class="display-1">Hospital</h1>
-        <h2 class="display-5">Universidad de El Salvador</h2>
-        <br>
-        <button type="button" class="btn btn-dark">
-          <a class="nav-link" href="./vistas/somos.html">Quienes Somos</a>
-        </button>
-        <br><br>
-      </div>
-    `;
-  
-    const sectionContainer = document.createElement('div');
-    sectionContainer.innerHTML = sectionHTML;
-    document.body.appendChild(sectionContainer);
-  }
-  
-  renderSection(); // Llamada a la función para que se ejecute al cargar la página
-  
-  
-  
+  const sectionHTML = `
+    <div class="principal-div text-center">
+      <br><br>
+      <h1 class="display-1">Hospital</h1>
+      <h2 class="display-5">Universidad de El Salvador</h2>
+      <br>
+      <button type="button" class="btn btn-dark">
+        <a class="nav-link" href="./somos.html">Sobre Nosotros</a>
+      </button>
+      <br><br>
+    </div>
+  `;
+
+  const sectionContainer = document.createElement('div');
+  sectionContainer.innerHTML = sectionHTML;
+  document.body.appendChild(sectionContainer);
+}
+
+renderSection(); // Llamada a la función para que se ejecute al cargar la página
+
+
