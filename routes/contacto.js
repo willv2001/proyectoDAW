d93a389207fce553ed72fc809d5eb393a7e98c7c
Hazel Azucena Calderón Bonilla <166912386+LezahA@users.noreply.github.com>
2024-05-24 11:43:05 -0600

    ajuste de envio de correos

--- routes/contacto.js
+++ routes/contacto.js
@@ -1,19 +1,50 @@
+
 const { Router } = require('express');
+const nodemailer = require('nodemailer');
+
 const router = Router();
 
-router.post('/enviarCorreo' , (req, res) => {
-    const { name, email, message } = req.body;
-
-    contentHtml = `
-        <h1>Informacion del usuario</h1>
-        <ul>
-            <li>Nombre: ${name}</li>
-            <li>Correo: ${email}</li>
-            <li>Mensaje: ${message}</li>
-        </ul>
-    `;
-    console.log(contentHtml);
-    res.send('Recibido');
+// Configurar el transporte de correo electrónico
+const transporter = nodemailer.createTransport({
+  service: 'gmail', // Servicio de correo electrónico
+  auth: {
+    user: 'haz.azu.calderonbonilla@gmail.com', // Tu dirección de correo electrónico
+    pass: 'lmkq shlc urjf rhxy' // Tu contraseña de correo electrónico
+  }
 });
 
-module.exports = router
+router.post('/enviarCorreo', (req, res) => {
+  const { name, email, message } = req.body;
+
+  const contentHtml = `
+    <h1>Información del usuario</h1>
+    <ul>
+      <li>Nombre: ${name}</li>
+      <li>Correo: ${email}</li>
+      <li>Mensaje: ${message}</li>
+    </ul>
+  `;
+
+  // Configurar los detalles del correo electrónico
+  const mailOptions = {
+    from: email, // Correo del remitente
+    to: 'haz.azu.calderonbonilla@gmail.com', // Correo del destinatario
+    subject: 'Contacto desde la web', // Asunto del correo
+    text: message,
+    // Cuerpo del correo en formato HTML
+    html: contentHtml // Cuerpo del correo en formato HTML
+  };
+
+  // Enviar el correo electrónico
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send('Error al enviar el correo');
+    } else {
+      console.log('Correo enviado: ' + info.response);
+      res.send('Correo enviado correctamente');
+    }
+  });
+});
+
+module.exports = router;
